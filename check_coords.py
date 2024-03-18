dc953a4ee2fb4c223e19f896a6e3049f9d6d45d0
timati19 <59282950+timati19@users.noreply.github.com>
2024-03-18 15:27:41 +0700

    2

--- check_coords.py
+++ check_coords.py
@@ -2,17 +2,11 @@ import requests
 
 
 def check(coords_orig, coords_app):
-    orig_request = f"http://static-maps.yandex.ru/1.x/?ll={coords_orig[0]},{coords_orig[1]}&spn=10,10&l=map"
-    app_request = f"http://static-maps.yandex.ru/1.x/?ll={coords_app[0]},{coords_app[1]}&spn=10,10&l=map"
-    orig_response = response = requests.get(orig_request)
-    app_response = response = requests.get(app_request)
-    if not orig_response or not app_response:
+    orig_request = f"http://static-maps.yandex.ru/1.x/?pt={coords_app[0]},{coords_app[1]},pmgrs2~{coords_orig[0]},{coords_orig[1]},pmgrs1&pl={coords_app[0]},{coords_app[1]},{coords_orig[0]},{coords_orig[1]}&l=map"
+    orig_response = requests.get(orig_request)
+    if not orig_response:
         return 'Ошибка'
     orig_map = 'orig_map.jpeg'
-    app_map = 'app_map.jpeg'
-    orig_file = open(orig_map, "wb")
     with open(orig_map, "wb") as file:
-        file.write(response.content)
-    with open(app_map, "wb") as file:
-        file.write(response.content)
-    return open(orig_map, "r"), open(app_map, "r")
+        file.write(orig_response.content)
+    return open(orig_map, "rb")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from config import TOKEN
+from get_distance import getDistance
 from check_coords import check
 
 token = TOKEN
@@ -8,15 +9,15 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=['location'])
 def start_message(message):
-    bot.send_message(message.chat.id, f"{message.location.latitude, message.location.longitude}")
+    msg = tuple([message.location.longitude, message.location.latitude])
+    print(56.475772, 84.949756, *msg)
+    img1 = check((84.949756, 56.475772,), msg)
+    bot.send_photo(message.chat.id, img1, caption=getDistance(56.475772, 84.949756, msg[1], msg[0]))
 
 
 @bot.message_handler(content_types=['text'])
 def start_message(message):
-    msg = [float(i) for i in message.text.split(', ')]
-    print((56.475772, 84.949756,), msg)
-    img1, img2 = check((56.475772, 84.949756,), (message.text))
-    bot.send_media_group(message.chat.id, [telebot.types.InputMediaPhoto(img1), telebot.types.InputMediaPhoto(img2)])
+   bot.send_message('ss')
 
 
 bot.infinity_polling()
